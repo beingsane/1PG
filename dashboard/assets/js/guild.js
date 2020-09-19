d1a3a224864bca0ce71c67e7499241211b6592ee
theADAMJR <adamjf123@gmail.com>
2020-09-19 16:10:33 +0100

    add working tabs

--- dashboard/assets/js/guild.js
+++ dashboard/assets/js/guild.js
@@ -1,11 +1,16 @@
-$('.tabs a').on('click', function() {
+$('.tabs a, #sidebarExtension h4').on('click', function() {
   $('.tabs a').removeClass('active');
-  $(this).addClass('active');
+
+  const selected = $(this);
+  selected.addClass('active');
+
+  const id = $(selected).attr('id');
+  setModule(id);
 });
 
-function setModule(name) {
+function setModule(moduleId) {
   $('.module').hide();
-  $(`#${name}Module`).show();
+  $(`#${moduleId}Module`).show();
 }
 
 setModule('overview');
--- dashboard/routes/dashboard-routes.js
+++ dashboard/routes/dashboard-routes.js
@@ -1,13 +1,28 @@
 const bot = require('../../bot');
 const express = require('express');
 const { validateGuild } = require('../middleware');
+const guilds = require('../../data/guilds');
 
 const router = express.Router();
 
 router.get('/dashboard', (req, res) => res.render('dashboard/index'));
 
-router.get('/servers/:id', validateGuild, (req, res) => res.render('dashboard/show', {
-  guild: bot.guilds.cache.get(req.params.id)
-}));
+router.get('/servers/:id', validateGuild, async (req, res) => {
+  const id = req.params.id;
+  res.render('dashboard/show', {
+    guild: bot.guilds.cache.get(id),
+    savedGuild: await guilds.get(id)
+  });
+});
+
+router.put('/servers/:id/:module', validateGuild, async (req, res) => {
+  const { id, module } = req.params;
+
+  const savedGuild = await guilds.get(id);
+  savedGuild[module] = req.body;
+  await savedGuild.save();
+  
+  res.redirect(`/servers/${id}`);
+});
 
 module.exports = router;
